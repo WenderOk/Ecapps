62a2599a4510838f0662e0aeec2cda0c0ef19f83
Leont1cov <chuchunovvakov@mail.ru>
2025-11-14 17:47:58 +0300

    feat: added react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import "./styles/_base.scss";
 import WelcomePage from "./components/layout/WelcomePage/WelcomePage.tsx";
+import {Route, Routes} from "react-router-dom";
 
+// TODO: написать компонент бизнес
 export default function App () {
     return (
         <>
-            <WelcomePage />
+            <Routes>
+                <Route path="/" element={<WelcomePage />}/>
+            </Routes>
         </>
     )
 }
