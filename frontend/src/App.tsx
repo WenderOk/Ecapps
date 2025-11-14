24a9677a7f5084b09d9fd01c163605aeeda73ef6
Leont1cov <chuchunovvakov@mail.ru>
2025-11-14 18:27:59 +0300

    feat: added ProfilePage file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import "./styles/_base.scss";
 import WelcomePage from "./components/layout/WelcomePage/WelcomePage.tsx";
 import {Route, Routes} from "react-router-dom";
+import ProfilePage from "./components/layout/ProfilePage/ProfilePage.tsx";
 
-// TODO: написать компонент бизнес
 export default function App () {
     return (
         <>
             <Routes>
                 <Route path="/" element={<WelcomePage />}/>
+                <Route path="/profile" element={<ProfilePage/>}/>
             </Routes>
         </>
     )
---
+++ frontend/src/components/layout/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,9 @@
+const ProfilePage = () => {
+    return (
+        <>
+            <h1>Тут будет @username</h1>
+        </>
+    )
+}
+
+export default ProfilePage;
