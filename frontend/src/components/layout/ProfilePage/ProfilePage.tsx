1a1b7c68f0d626947319af4ade408b73b715bbb0
Leont1cov <chuchunovvakov@mail.ru>
2025-11-15 11:48:35 +0300

    feat: added Profile page component

--- frontend/src/components/layout/ProfilePage/ProfilePage.tsx
+++ frontend/src/components/layout/ProfilePage/ProfilePage.tsx
@@ -1,7 +1,28 @@
+import QRCodeGenerator from "../../QRCode/QRCodeGenerator.tsx";
+import "./ProfilePage.scss";
+import ActionButton from "../../ui/ActionButton/ActionButton.tsx";
+import {faQrcode, faTags} from "@fortawesome/free-solid-svg-icons";
+import {Link} from "react-router-dom";
+
 const ProfilePage = () => {
+    const profileUrl = `${window.location.origin}/profile/12345`;
+
     return (
         <>
-            <h1>Тут будет @username</h1>
+            <div className="ProfilePage">
+                <h1 className="ProfilePage__username">username</h1>
+                <div className="ProfilePage__QRCode">
+                    <QRCodeGenerator value={profileUrl} />
+                </div>
+
+                <div className="ProfilePage__actions">
+                    <ActionButton title="Скидки" icon={faTags} />
+
+                    <Link to="/profile" style={{ textDecoration: "none" }}>
+                        <ActionButton title="QR" icon={faQrcode} />
+                    </Link>
+                </div>
+            </div>
         </>
     )
 }
