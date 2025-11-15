0a6920af2cf711727171d22595bb9b4efc13e80e
Leont1cov <chuchunovvakov@mail.ru>
2025-11-15 13:21:36 +0300

    feat: added OffersPage component

---
+++ frontend/src/components/layout/OffersPage/OffersPage.tsx
@@ -0,0 +1,12 @@
+import Card from "../../ui/Card/Card.tsx";
+
+const OffersPage = () => {
+    return (
+        <>
+            <Card title="Вкусно и точка" subtitle="lorem ipsum" image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fapi.eric.s3storage.ru%2Fsuper-static%2Fprod%2F6331b00b3f0e357ed89b7ce9-1900x.jpeg&f=1&nofb=1&ipt=3fd238aa7e8a00fe3fdcbe5f0d32d6ab0f9def0bd1cb2acd7ee66a486aea46a2" />
+            <Card title="Буквоед" subtitle="Скидка 90% на всё всё всё! lorem ipsum" address="Мира 8А Выборг Выборгский район Ленинградская область" image="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fkaliningradplaza.ru%2Fimages%2Fcms%2Fdata%2Fnews%2F2018%2F13.jpg&f=1&nofb=1&ipt=cf6e1f972309de717ec1d33854b2a2e434ab05d8b8d2da2ed1eac921e56575f3"/>
+        </>
+    )
+}
+
+export default OffersPage;
