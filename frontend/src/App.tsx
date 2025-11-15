7fa3bd338ec2c920b5abaa992916aff166359ae6
Leont1cov <chuchunovvakov@mail.ru>
2025-11-15 13:22:36 +0300

    feat: added links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import "./styles/_base.scss";
 import WelcomePage from "./components/layout/WelcomePage/WelcomePage.tsx";
 import {Route, Routes} from "react-router-dom";
 import ProfilePage from "./components/layout/ProfilePage/ProfilePage.tsx";
+import OffersPage from "./components/layout/OffersPage/OffersPage.tsx";
 
 export default function App () {
     return (
         <>
             <Routes>
                 <Route path="/" element={<WelcomePage />}/>
-                <Route path="/profile" element={<ProfilePage/>}/>
+
+                <Route path="/profile" element={<ProfilePage />}/>
+                <Route path="/profile/offers" element={<OffersPage />} />
             </Routes>
         </>
     )
