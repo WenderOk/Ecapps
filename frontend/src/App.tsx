493b802068789559d8cfc0faf845127837f5c26f
Leont1cov <chuchunovvakov@mail.ru>
2025-11-16 12:30:24 +0300

    refactor: written a protective component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import {Route, Routes} from "react-router-dom";
 import ProfilePage from "./components/layout/ProfilePage/ProfilePage.tsx";
 import OffersPage from "./components/layout/OffersPage/OffersPage.tsx";
 import AdminLogIn from "./components/layout/AdminLogIn/AdminLogIn.tsx";
+import AdminPanel from "./components/layout/AdminPanel/AdminPanel.tsx";
+import ProtectedRoute from "./components/router/ProtectedRoute.tsx";
 
 export default function App () {
     return (
@@ -11,6 +13,13 @@ export default function App () {
             <Routes>
                 <Route path="/" element={<WelcomePage />}/>
                 <Route path="/adminLogIn" element={<AdminLogIn/>}/>
+
+                <Route path="/adminPanel" element={
+                    <ProtectedRoute>
+                        <AdminPanel/>
+                    </ProtectedRoute>
+                }/>
+
                 <Route path="/profile" element={<ProfilePage />}/>
                 <Route path="/profile/offers" element={<OffersPage />} />
             </Routes>
